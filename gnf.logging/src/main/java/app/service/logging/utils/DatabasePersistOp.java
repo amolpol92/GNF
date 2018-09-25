c53fc57d0dca35d10ff5590e15fd45934995be05
amolp <amolp@GPTC15.bitwiseglobal.net>
2018-09-25 16:26:16 +0530

    Logging and db persist

--- gnf.logging/src/main/java/app/service/logging/utils/DatabasePersistOp.java
+++ gnf.logging/src/main/java/app/service/logging/utils/DatabasePersistOp.java
@@ -5,32 +5,25 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import app.service.logging.model.LogRequest;
-import app.service.logging.utils.ConfigParams;
-import app.service.logging.utils.PropertyParserAndConfigAdapter;
 
 public class DatabasePersistOp {
-	private PropertyParserAndConfigAdapter adapter;
-	private ConfigParams params;
-	private LogRequest logDataContainer;
+	private Connection connection;
 
-	public DatabasePersistOp(LogRequest logRequest) {
-		// TODO Auto-generated constructor stub
-		this.logDataContainer = logRequest;
-		this.adapter = new PropertyParserAndConfigAdapter("logging_configuration.properties");
-		this.params = this.adapter.readPropertiesAndSetParameters();
-	}
+	public DatabasePersistOp() throws SQLException {
+		DBConnectionProvider connProvider = new DBConnectionProvider();
+		this.connection = connProvider.getConnection();
+			}
 
-	public void persistIntoDatabase() {
+	public void persistIntoDatabase(LogRequest logRequest) {
 		//try with resources; Connection object
-		try(Connection connection = this.params.getConn()) {
-			System.out.println(connection.getAutoCommit());
-			String sqlQueryString = "insert into log_details (glo_tran_id, severity, mntrd_res_typ, log_msg, log_name) VALUES (?, ?, ?, ?, ?)";
-			PreparedStatement statement = connection.prepareStatement(sqlQueryString);
-			statement.setString(1, this.logDataContainer.getLabels().get("GlobalTxnId"));
-			statement.setString(2, this.logDataContainer.getSeverity());
-			statement.setString(3, this.logDataContainer.getMonitoredResource());
-			statement.setString(4, this.logDataContainer.getMessage());
-			statement.setString(5, this.logDataContainer.getLogName());
+		String sqlQueryString = "insert into log_details (glo_tran_id, severity, mntrd_res_typ, log_msg, log_name) VALUES (?, ?, ?, ?, ?)";
+		try(PreparedStatement statement = connection.prepareStatement(sqlQueryString)) {
+			
+			statement.setString(1, logRequest.getLabels().get("GlobalTxnId"));
+			statement.setString(2, logRequest.getSeverity());
+			statement.setString(3, logRequest.getMonitoredResource());
+			statement.setString(4, logRequest.getMessage());
+			statement.setString(5, logRequest.getLogName());
 			statement.execute();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
